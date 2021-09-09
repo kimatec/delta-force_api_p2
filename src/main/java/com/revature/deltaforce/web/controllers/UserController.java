c119c0590fd5c1e33b4052eff091254ea720e475
hpuf <csmcdn@gmail.com>
2021-09-08 19:54:22 -0500

    combined register into usercontroller

--- src/main/java/com/revature/deltaforce/web/controllers/UserController.java
+++ src/main/java/com/revature/deltaforce/web/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.revature.deltaforce.web.controllers;
 
+import com.revature.deltaforce.datasources.models.AppUser;
 import com.revature.deltaforce.services.UserService;
 import com.revature.deltaforce.web.dtos.AppUserDTO;
+import com.revature.deltaforce.web.dtos.Principal;
 import com.revature.deltaforce.web.util.security.Secured;
 import com.revature.deltaforce.web.util.security.TokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.Set;
 
 @RestController
@@ -14,18 +18,28 @@ import java.util.Set;
 public class UserController {
 
     UserService userService;
+    TokenGenerator tokenGenerator;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, TokenGenerator tokenGenerator) {
         this.userService = userService;
+        this.tokenGenerator = tokenGenerator;
     }
 
+    // Get a user by their ID
     @GetMapping(value = "{id}", produces = "application/json")
     public AppUserDTO getUserById(@PathVariable String id) {
         return userService.findUserById(id);
     }
 
-    // Not sure if I like the way I implemented these, will probably make a UserFavesDTO later instead - cody
+    // For registering a new user
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Principal registerNewUser(@RequestBody @Valid AppUser newUser, HttpServletResponse resp) {
+        Principal principal = new Principal(userService.registerNewUser(newUser));
+        resp.setHeader(tokenGenerator.getJwtHeader(), tokenGenerator.createToken(principal));
+        return principal;
+    }
+
     // user/23i4on3ad4sd3fi3oj/faves?add=eggs
     @PostMapping(
             value = "{id}/faves",
