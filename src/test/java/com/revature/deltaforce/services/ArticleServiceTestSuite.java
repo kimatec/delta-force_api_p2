b909495a989680068048c06c2b13a31ca630c067
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-09-13 21:32:55 -0400

    ArticleService 100% method coverage

--- src/test/java/com/revature/deltaforce/services/ArticleServiceTestSuite.java
+++ src/test/java/com/revature/deltaforce/services/ArticleServiceTestSuite.java
@@ -1,5 +1,6 @@
 package com.revature.deltaforce.services;
 
+import com.revature.deltaforce.datasources.models.AppUser;
 import com.revature.deltaforce.datasources.models.Comment;
 import com.revature.deltaforce.datasources.models.DeltaArticle;
 import com.revature.deltaforce.datasources.models.ExternalAPIArticle;
@@ -16,9 +17,8 @@ import org.springframework.test.context.TestPropertySource;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -39,21 +39,23 @@ public class ArticleServiceTestSuite {
     @AfterEach
     public void afterEachTest() { sut = null; }
 
-    // newsResponseHandler tests //TODO: Add mockRepo to these tests
-//    @Test
-//    public void newsResponseHandler_returnsDeltaArticles_whenNewsAPI_respondsAsExpected() throws MalformedURLException {
-//        // Arrange
-//        List<ExternalAPIArticle> validRespList = new ArrayList<>(Arrays.asList(new ExternalAPIArticle()));
-//        List<DeltaArticle> expectedResult = new ArrayList<>(Arrays.asList(new DeltaArticle()));
-//        List<URL> urlList = new ArrayList<>(Arrays.asList(new URL("url")));
-//
-//        when(mockArticleRepo.findDeltaArticleByUrl(urlList)).thenReturn()
-//        // Act
-//        List<DeltaArticle> actualResult = sut.newsResponseHandler(validRespList);
-//
-//        // Assert
-//        assertEquals(expectedResult,actualResult);
-//    }
+
+    @Test
+    public void newsResponseHandler_returnsDeltaArticles_whenNewsAPI_respondsAsExpected() throws MalformedURLException {
+        // Arrange
+        List<ExternalAPIArticle> validRespList = new ArrayList<>(Arrays.asList(new ExternalAPIArticle()));
+        DeltaArticle expectedArticle = new DeltaArticle();
+        List<DeltaArticle> expectedResult = new ArrayList<>(Arrays.asList(expectedArticle));
+        List<URL> urlList = new ArrayList<>(Arrays.asList(new URL("https://newsapi.org/v2/")));
+
+        when(mockArticleRepo.findDeltaArticleByUrl(urlList)).thenReturn(expectedResult);
+        when(mockArticleRepo.findArticleByUrl(any())).thenReturn(expectedArticle);
+        // Act
+        List<DeltaArticle> actualResult = sut.newsResponseHandler(validRespList);
+
+        // Assert
+        assertEquals(expectedResult,actualResult);
+    }
 
     @Test
     public void newsResponseHandler_throwsException_whenNoNewsIsReceived(){
@@ -166,5 +168,60 @@ public class ArticleServiceTestSuite {
         assertFalse(a2HasLike);
         assertFalse(a2HasComment);
 
+    }
+
+    @Test
+    public void getPopularArticles_returnsAListofTenArticles_whenCalled(){
+        //Arrange
+        List<DeltaArticle> input = new ArrayList<>();
+        for(int i=0; i<20; i++)
+            input.add(new DeltaArticle());
+        when(mockArticleRepo.findAll()).thenReturn(input);
+        //Act
+        List<DeltaArticle> actualResult = sut.getPopularArticles();
+        //Assert
+        assertEquals(10, actualResult.size());
+    }
+
+    @Test
+    public void getFavoriteUrls_returnsCorrectList_whenFavTopicsIsNotEmpty(){
+        //Arrange
+        AppUser dummy = new AppUser("first","last","email","username","password",new HashSet<>(Arrays.asList("TOPIC")));
+        String expected = "top-headlines?country=us&category=TOPIC&apiKey=";
+        when(mockUserRepo.findAppUserByUsername(dummy.getUsername())).thenReturn(dummy);
+
+        //Act
+        List<String> returnedList = sut.getFavoriteUrls(dummy.getUsername());
+        String actual = returnedList.get(0);
+
+        //Assert
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void updateUsername_updatesUsername_whenCalled(){
+        //Arrange
+        AppUser dummy = new AppUser("valid", "valid", "valid", "username", "valid");
+        Comment comment = new Comment("valid", dummy.getUsername(), "valid");
+        List<String> usernameList = new ArrayList<>(Arrays.asList(dummy.getUsername()));
+        DeltaArticle dummyArticle = new DeltaArticle();
+        List<DeltaArticle> queryResult = new ArrayList<>(Arrays.asList(dummyArticle));
+
+        dummyArticle.setLikes(usernameList);
+        dummyArticle.setDislikes(usernameList);
+        dummyArticle.setComments(new ArrayList<>(Arrays.asList(comment)));
+
+        when(mockArticleRepo.findDeltaArticleByUsername(dummy.getUsername())).thenReturn(queryResult);
+        when(mockArticleRepo.saveAll(any())).thenAnswer(i -> i.getArguments()[0]);
+        //Act
+        List<DeltaArticle> actualResult = sut.updateUsername("username", "updated");
+        DeltaArticle actualArticle = actualResult.get(0);
+        String commentResult = actualArticle.getComments().get(0).getUsername();
+        String likesResult = actualArticle.getLikes().get(0);
+        String dislikesResult = actualArticle.getDislikes().get(0);
+        //Assert
+        assertEquals("updated", commentResult);
+        assertEquals("updated", likesResult);
+        assertEquals("updated", dislikesResult);
     }
 }
