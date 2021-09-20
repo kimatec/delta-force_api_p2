3bc99e120c1c9ea863de5c22eb96851bbdf179d5
Hiroshi Nobuoka <hiroshi.nobuoka@gmail.com>
2021-09-20 17:57:16 -0400

    more sorting

--- src/main/java/com/revature/deltaforce/web/controllers/NewsController.java
+++ src/main/java/com/revature/deltaforce/web/controllers/NewsController.java
@@ -72,12 +72,11 @@ public class NewsController {
                 .map(url -> restClient.getForObject(url, NewsResponse.class).getArticles())
                 .map(articleService::newsResponseHandler)
                 .flatMap(list -> list.stream())
-                .sorted()
                 .collect(Collectors.toList());
 
         //If user has favorite topics, shuffle the headlines so the feed will show articles from different categories.
         if (!favTopicUrls.contains("top-headlines?country=us&apiKey="))
             Collections.shuffle(favArticles);
-        return favArticles.subList(0, 9);
+        return favArticles.subList(0, 9).stream().sorted().collect(Collectors.toList());
     }
 }
